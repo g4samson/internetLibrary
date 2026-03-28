00186c739cf90701b30a46580d2e7efc20089f79
g4samson <grigsam14@gmail.com>
2026-03-28 20:20:05 +0300

    Initial

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -1,84 +1,84 @@
-plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    id("maven-publish")
-
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17"
-    id("com.google.dagger.hilt.android")
-}
-
-android {
-    namespace = "com.test.mylibrary"
-    compileSdk {
-        version = release(36)
+    plugins {
+        alias(libs.plugins.android.library)
+        alias(libs.plugins.kotlin.android)
+        id("maven-publish")
     }
 
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+    android {
+        namespace = "com.test.mylibrary"
+        compileSdk {
+            version = release(36)
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    publishing {
-        singleVariant("release") {
-            withJavadocJar()
-        }
-    }
-}
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            afterEvaluate {
-                from(components["release"])
+        defaultConfig {
+            minSdk = 24
+
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+            consumerProguardFiles("consumer-rules.pro")
+        }
+
+        buildFeatures {
+            compose = true
+        }
+        composeOptions {
+            kotlinCompilerExtensionVersion = "1.5.15"
+        }
+
+        buildTypes {
+            release {
+                isMinifyEnabled = false
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
             }
-            groupId = "com.example.g4samson"
-            artifactId = "internetLibrary"
-            version = "1.0.0"
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+        publishing {
+            singleVariant("release") {
+                withJavadocJar()
+            }
         }
     }
-}
 
-dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                afterEvaluate {
+                    from(components["release"])
+                }
+                groupId = "com.example.g4samson"
+                artifactId = "internetLibrary"
+                version = "1.0.0"
+            }
+        }
+    }
 
-    implementation("com.google.dagger:hilt-android:2.57.1")
-    ksp("com.google.dagger:hilt-android-compiler:2.57.1")
+    dependencies {
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.lifecycle.runtime.ktx)
+        implementation(libs.androidx.activity.compose)
+        implementation(platform(libs.androidx.compose.bom))
+        implementation(libs.androidx.compose.ui)
+        implementation(libs.androidx.compose.ui.graphics)
+        implementation(libs.androidx.compose.ui.tooling.preview)
+        implementation(libs.androidx.compose.material3)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
+        androidTestImplementation(platform(libs.androidx.compose.bom))
+        androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+        debugImplementation(libs.androidx.compose.ui.tooling)
+        debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    // Retrofit with Scalar Converter
-    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
-}
+        // Retrofit
+        implementation("com.squareup.retrofit2:retrofit:2.9.0")
+        // Retrofit with Scalar Converter
+        implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    }
--- mylibrary/src/main/java/com/test/mylibrary/domain/repository/DomainRepositoryImpl.kt
+++ mylibrary/src/main/java/com/test/mylibrary/domain/repository/DomainRepositoryImpl.kt
@@ -1,9 +1,10 @@
 package com.test.mylibrary.domain.repository
 
 import com.test.mylibrary.data.utils.Provider
-import javax.inject.Inject
+//import javax.inject.Inject
 
-class DomainRepositoryImpl @Inject constructor() : DomainRepository {
+class DomainRepositoryImpl //@Inject constructor()
+    : DomainRepository {
     val retrofitApi get() = Provider.provideRetrofit()
 
     override suspend fun ping(): String {
